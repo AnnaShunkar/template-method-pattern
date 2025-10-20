d3281d234ae29f38dc29bf8fddb54d69735c8ac8
AnnaShunkar <anna.shunkar4@gmail.com>
2025-10-20 20:20:40 +0300

    Add files via upload

---
+++ template-method-pattern.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+class AbstractProcess {
+public:
+    void run() {
+        step1();
+        step2();
+        commonStep();
+    }
+    virtual void step1() = 0;
+    virtual void step2() = 0;
+    void commonStep() {
+        cout << "Common step executed." << endl;
+    }
+    virtual ~AbstractProcess() = default;
+};
+//реалізація A//
+class ConcreteA : public AbstractProcess {
+public:
+    void step1() override {
+        cout << "ConcreteA step1" << endl;
+    }
+    void step2() override {
+        cout << "ConcreteA step2" << endl;
+    }
+};
+//реалізація B//
+class ConcreteB : public AbstractProcess {
+public:
+    void step1() override {
+        cout << "ConcreteB step1" << endl;
+    }
+    void step2() override {
+        cout << "ConcreteB step2" << endl;
+    }
+};
+int main() {
+    ConcreteA processA;
+    ConcreteB processB;
+
+    processA.run(); 
+    processB.run(); 
+    return 0;
+}
